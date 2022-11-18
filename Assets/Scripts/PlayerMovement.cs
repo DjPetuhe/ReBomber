83b4d585207c12bdf92696a9d398504cc1990deb
DjPetuhe <validhernufKPI@gmail.com>
2022-11-18 04:11:35 +0300

    Added Bomberman's movement and animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float epsilon = 0.001f;
+
+    [SerializeField]
+    private Transform bomberman;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float speed = 2.5f;
+
+    private Vector2 direction;
+    private int _dir = 1;
+    public int Dir
+    {
+        get { return _dir; }
+        private set
+        {
+            _dir = value;
+            transform.localScale = _dir == 4 ? new Vector3(-1, 1, 1) : new Vector3(1, 1, 1);
+        }
+    }
+
+    private int FindDirection()
+    {
+        if (direction.sqrMagnitude > epsilon)
+        {
+            if (direction.x > direction.y) Dir = (direction.x > -direction.y) ? 2 : 1;
+            else Dir = (direction.x < -direction.y) ? 4 : 3;
+        }
+        return Dir;
+    }
+
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        direction.Normalize();
+
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", direction.magnitude);
+        animator.SetInteger("Direction", FindDirection());
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + speed * Time.fixedDeltaTime * direction);
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ReBomber : coursework
 
-## This is a coursework repository for Igor Sikorsky Kyiv Polytechnic Institue.  
+### This is a coursework repository for Igor Sikorsky Kyiv Polytechnic Institue.
 
 Topic: An update of the classic game "Bomberman" with the development of a level editor.  
 Student: Hernuf Valid.  
